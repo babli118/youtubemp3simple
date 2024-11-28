01d72eeee9098241afa66b507d421345f3a4d450
babli123 <valorantturk460@gmail.com>
2024-11-28 23:44:45 +0500

    refactored

--- components/ytDlInfo.jsx
+++ components/ytDlInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const YtDlInfo = () => {
   return (
-    <div className="text-text text-start mx-auto  items-center  justify-center md:w-[77vw] xl:w-[50vw] w-[90vw] ">
+    <div className="text-text text-start mx-auto  items-center  justify-center">
       <p className=" mt-4 text-sm sm:text-base text-text leading-7 sm:leading-7">
         YOUTUBEMP3 is the fastest Youtube To MP3 Converter that allows you to
         easily convert youtube to MP3 and MP4 from youtube for free and in the
